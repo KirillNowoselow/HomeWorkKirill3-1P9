15f49e3ce79ed3019b719218cab1cf5c64ca8992
kirill <kirillnowoselow4@gmail.com>
2023-01-16 22:23:29 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkKirill3-1P9/RentalFee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore
+{
+    internal class RentalFee
+    {
+        public MovieCategory MovieCategory { get; set; }
+        public MediaType MediaType { get; set; }
+    }
+}
